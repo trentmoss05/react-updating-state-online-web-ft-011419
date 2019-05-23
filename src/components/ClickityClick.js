ef656417f44ae89f9640b0b6078861bc64e49ca8
Trent Moss <trentmoss05@gmail.com>
2019-05-23 02:42:15 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -27,10 +27,3 @@ class ClickityClick extends React.Component {
  
 export default ClickityClick;
  
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
- 
-import ClickityClick from './components/ClickityClick';
- 
-ReactDOM.render(<ClickityClick />, document.getElementById('root'));
